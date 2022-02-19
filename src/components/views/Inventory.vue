2cbd81dafce1302d61085b23d248f43d221d07ab
developerazad <azad@tst.com>
2022-02-19 21:09:10 +0600

    fetch inventory from api server

--- src/components/views/Inventory.vue
+++ src/components/views/Inventory.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row">
+    <div v-if="!loading" class="row">
         <div v-for="(item, index) in items" :key="index" class="card" style="width: 15rem;">
             <img :src="item.photo" class="card-img-top" alt="cart-img">
             <div class="card-body">
@@ -9,19 +9,36 @@
             </div>
         </div>
     </div>
+    <div v-else class="row">
+        <h3>Loading...</h3>
+    </div>
 </template>
 
 <script>
+
+import axios from 'axios'
 export default {
-    //props:['items'],
     data(){
         return {
+            loading: true,
             items: []
         }
+    },
+    mounted() {
+        this.fetchInventory()
     },
     methods:{
         addItemToCart(item){
             this.$emit('newItemAdded', item)
+        },
+        fetchInventory(){
+            var self = this
+            axios.get('http://localhost:3000/items').then(response => {
+                setTimeout(function(){
+                    self.items = response.data
+                    self.loading = false
+                }, 3000)                
+            })
         }
     }
 }
